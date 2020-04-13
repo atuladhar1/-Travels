a0c3d5195b58443a39154a0679855d24f13fe04f
atuladhar1 <atuladhar1@gmail.com>
2020-04-13 16:29:33 -0500

    Started login page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login or Sign up</title>
+</head>
+<body>
+    <div>
+    <h1>
+        Login
+    </h1>
+    <form id = "login">
+    <label for ="uname">Username </label>
+    <input type ="text" id= "uname" name="uname">
+    <label for ="password">Password</label>
+    <input type ="password" id="password" name="password">
+    </form>
+    </div>
+    <h1>Sign up</h1>
+    <label for = "fname"> First Name</label>
+    <input type = "text" id ="fname" name= "fname">
+    <label for = "lname"> Last Name</label>
+    <input type = "text" id = "lname" name= "lname">
+    <label for = "username"> Username </label>
+    <input type = "text" id = username name = "username">
+    <input type = 
+
+   
+</body>
+</html>
